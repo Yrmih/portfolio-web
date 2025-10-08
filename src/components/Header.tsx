ee468fcf43b5f6f0fd8e07da931c8d342b5ee6e0
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-08 00:48:34 -0300

    feat: adicionar links de navegação ao componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,21 @@
 
 
 export default function Header(){
+
+  const links = [
+    {name: "Início", href: "#home"},
+    {name: "Sobre", href: "#about"},
+    {name: "Skills", href: "#skills"},
+    {name: "Projetos", href: "#projects"},
+    {name: "Contato", href: "#contact"},
+  ];
+
+
   return(
     <header className="fixed w-full bg-gray-900/90 backdrop-blur-md z-50">
+      <nav>
 
+      </nav>
     </header>
   );
 }
