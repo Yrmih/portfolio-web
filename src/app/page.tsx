6a2d00266609f0bce4e59d63517e6f5eaeaf3802
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-08 00:14:19 -0300

    feat: adicionar estrutura inicial da página e componentes Header, Footer, NavLink e Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+
+import Header from "@/components/Header";
+import Hero from "@/sections/Hero";
+import About from "@/sections/About";
+import Skills from "@/sections/Skills";
+import Projects from "@/sections/Projects";
+import Contact from "@/sections/Contact";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <>
+    
+    
+    
+    </>
   );
 }
