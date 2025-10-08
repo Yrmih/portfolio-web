990620ace5f3a84d4742f8b89c34c6073b8085d0
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-08 00:56:04 -0300

    refactor: ajustar formatação e estrutura do componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,32 @@
-"use client"; 
+"use client";
+import { link } from "fs";
+import { useState } from "react";
 
+export default function Header() {
 
-export default function Header(){
-
+  const [isOpen, setIsOpen] = useState(false);
   const links = [
-    {name: "Início", href: "#home"},
-    {name: "Sobre", href: "#about"},
-    {name: "Skills", href: "#skills"},
-    {name: "Projetos", href: "#projects"},
-    {name: "Contato", href: "#contact"},
+    { name: "Início", href: "#home" },
+    { name: "Sobre", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projetos", href: "#projects" },
+    { name: "Contato", href: "#contact" },
   ];
 
-
-  return(
+  return (
     <header className="fixed w-full bg-gray-900/90 backdrop-blur-md z-50">
-      <nav>
-
+      <nav className="max-w-6xl mx-auto flex justify-between items-center p-4">
+        <div className="text-2xl font-bold"> Yrmih Ian</div>
+        <ul className="hidden md:flex space-x-6">
+          {links.map((link) => (
+            <li key={link.href}>
+              <a href={link.href} className="hover:text-blue-500"></a>
+            </li>
+          ))}
+        </ul>
+          <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+             ☰
+          </button>
       </nav>
     </header>
   );
