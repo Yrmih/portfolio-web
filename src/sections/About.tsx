17071e30088a54728ee60f473db5aa46db7e9eae
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-10 14:17:19 -0300

    feat: adicionar seção "Sobre mim" ao componente About

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,8 @@
 export default function About(){
   return(
-    <div></div>
+   <section>
+    <h2>Sobre mim</h2>
+    <p>Sou desenvolvedor Fullstack, focado em construir aplicações modernas e escaláveis utilizando TypeScript. No meu trabalho, utilizo uma variedade de frameworks para o front-end e o back-end, garantindo a melhor solução para cada projeto. Tenho experiência com tecnologias como Angular, React, Node.js, Express, entre outros, sempre buscando otimizar o desempenho e garantir uma boa experiência para o usuário final.</p>
+   </section>
   );
 }
