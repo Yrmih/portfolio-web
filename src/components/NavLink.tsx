6d80e9322aebdabd26aef43950eb8d8fdd6fb8ec
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-11 10:54:01 -0300

    feat: adicionar interface e props ao componente NavLink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,7 +1,16 @@
-export default function NavLink() {
-  return (
-    <a href="">
-      
-    </a>
-  )
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function NavLink({href, children}: NavLinkProps) {
+
+  const [ active, setActive ] = useState(false);
+
+  
+  return <a href=""></a>;
 }
