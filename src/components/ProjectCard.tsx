f360f4cac4feb8d822058ddbea070c3ecbc592f6
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-19 01:18:03 -0300

    feat - criando o escopo de project card

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Project } from "@/types";
+
+interface ProjectCardProps {
+  project: Project;
+  index: number;
+}
+
+export default function ProjectCard({project, index}: ProjectCardProps){
+
+}
