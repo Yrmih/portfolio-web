3a3bee03e751ac8a57d81f692108db79e1d3d9e8
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-19 18:57:33 -0300

    feat - implementando services e framer motion no mesmo componente

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,3 +1,15 @@
+import { motion } from "framer-motion";
+
+
+
 export default function Services() {
-  return
+  return (
+    <section className="py-20 bg=[#0a0a0f] text-gray-200">
+      <div className="max-w-6xl mx-auto px-6">
+        <motion.h2>
+
+        </motion.h2>
+      </div>
+    </section>
+  );
 }
