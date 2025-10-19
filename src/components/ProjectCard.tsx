0a38e02305231a42adbb401225086e0ea7f58c8e
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-19 01:20:52 -0300

    feat - adicionando motion da lib framer motio

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,5 +10,13 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard({project, index}: ProjectCardProps){
-
+  return(
+    <motion.div
+      className="bg-gray-800 rounded-2xl overflow-hidden shadow-lg hover:shadow-blue-500/20 transition-all duration-300"
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{ delay: index * 0.15, duration: 0.6 }}
+    ></motion.div>
+  )
 }
