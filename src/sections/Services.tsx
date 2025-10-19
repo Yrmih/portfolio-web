43066b45407a18fd5a722722ca404bd0fcc3c5be
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-10-19 19:15:19 -0300

    feat - preenhendo conteudos de serviços no componente de services

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,6 +1,25 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { Code, Layout, Smartphone, Database, Server, Cloud, Lock, Wrench, Cpu } from "lucide-react";
+
+const services = [
+  {
+    icon: <Layout className="w-6 h-6 text-blue-400" />,
+    title: "Desenvolvimento de Interfaces",
+    desc: "Criação de interfaces modernas, responsivas e acessíveis.",
+  },
+  {
+    icon: <Smartphone className="w-6 h-6 text-purple-400" />,
+    title: "Design Responsivo",
+    desc: "Layout otimizado para qualquer dispositivo ou tela.",
+  },
+  {
+    icon: <Code className="w-6 h-6 text-pink-400" />,
+    title: "Componentes Reutilizáveis",
+    desc: "Desenvolvimento modular e otimizado com React e ShadCN.",
+  },
+]
 
 export default function Services({ id }: { id: string }) {
   return (
@@ -14,7 +33,9 @@ export default function Services({ id }: { id: string }) {
         >
           Meus Serviços
         </motion.h2>
-        <div></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {services.map}
+        </div>
       </div>
     </section>
   );
